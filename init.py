0771f4ed0eed0f0fa3ce5cf0c9cd6d5c3ccbe3be
Fira-Slavu Rareș-Florin <raresdragutuu@gmail.com>
2025-10-15 09:52:58 +0300

    app that responds to requests at the root, /articles and /articles/:id

---
+++ init.py
@@ -0,0 +1,17 @@
+from flask import Flask, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'Welcome!'
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<article_id>')
+def api_article(article_id):
+    return 'You are reading ' + article_id
+
+if __name__ == "__main__":
+    app.run()
