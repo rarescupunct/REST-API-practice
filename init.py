72b7014f54eb6d422abac9a97a8a8d7ac15cb849
Fira-Slavu Rareș-Florin <raresdragutuu@gmail.com>
2025-10-20 08:54:07 +0300

    same as the last commit, but now responds differently based on http verbs

--- init.py
+++ init.py
@@ -2,12 +2,22 @@ from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def api_hello():
-    if "name" in request.args:
-        return f"Hello, {request.args['name']}!"
-    else:
-        return "Hello, Ragazzi!"
+@app.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO:GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO:POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO:PATCH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO:PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO:DELETE\n"
 
 if __name__ == "__main__":
     app.run()
